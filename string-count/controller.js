0f82040486ec35e2e84a490d89aa141be4e5f8d6
Marcos Savoury <marcossavoury@hotmail.com>
2015-07-03 16:40:12 -0400

    adding functionality

--- string-count/controller.js
+++ string-count/controller.js
@@ -1,5 +1,27 @@
 var app = angular.module('app', []);
-app.controller('appCtrl', ['$scope', function ($scope) {
-    $scope.dog = {name: 'poodler'};
-	$scope.text = "";
-}]);
+app.directive('limitBox', function() {
+
+	return {
+		restrict: 'E',
+		scope: {},
+		link: function(scope, elem, attr) {
+			scope.maxChars = attr.maxchars;
+		},
+		controller: function($scope) {
+			$scope.content = "";
+			$scope.percentConsumed = function() {
+				return parseInt($scope.content.length / $scope.maxChars * 100); 
+			};
+		},
+		template: 
+		'<div>' +
+		  '<textarea style="width:100%;" maxlength={{maxChars}} ng-model="content"></textarea>' +
+		  '<div class="progress">'+
+			  '<div class="progress-bar" role="progressbar" aria-valuenow="{{percentConsumed()}}" aria-valuemin="0" aria-valuemax="100" style="width: {{percentConsumed()}}%;">' +
+				  '<span class="sr-only">{{percentConsumed()}}% Complete</span>' +
+			  '</div>' +
+	      '</div>' +
+		'</div>'
+	};
+
+});
